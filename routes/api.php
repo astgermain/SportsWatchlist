3c0c07bf49b6e53b712002996ad100b6fd070bfe
Andrew St Germain <roarkyo@gmail.com>
2020-10-07 10:48:32 -0700

    Test finished

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
