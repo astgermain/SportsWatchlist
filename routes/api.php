a16cfcf7435401df689ca4bd90dd4cfc8b57b565
Andrew St Germain <roarkyo@gmail.com>
2020-10-07 10:38:29 -0700

    User test

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
 });
 
