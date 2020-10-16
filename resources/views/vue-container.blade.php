cf8467562188472781b741d02cbdbee0f0dd79ef
Andrew St Germain <roarkyo@gmail.com>
2020-10-16 09:25:19 -0700

    Added Facebook Pixel

--- resources/views/vue-container.blade.php
+++ resources/views/vue-container.blade.php
@@ -38,6 +38,25 @@
     <script src="{{ mix('js/app.js') }}" defer></script>
     <!-- Global site tag (gtag.js) - Google Ads: 581289988 --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-581289988"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-581289988'); </script>
     <!-- Event snippet for Landing Page Page view conversion page --> <script> gtag('event', 'conversion', {'send_to': 'AW-581289988/XJlpCKrltOEBEISQl5UC'}); </script>
+    <!-- Facebook Pixel Code -->
+      <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window,document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '656829345258816'); 
+      fbq('track', 'PageView');
+      </script>
+      <noscript>
+      <img height="1" width="1" 
+      src="https://www.facebook.com/tr?id=656829345258816&ev=PageView
+      &noscript=1"/>
+      </noscript>
+    <!-- End Facebook Pixel Code -->
   </head>
   <body>
     <div id="app"></div>
